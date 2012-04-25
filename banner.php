dc3e692282f33008b84f733682f81fc6c7374c6d
aligh <aghasizadeh70@gmail.com>
2012-04-25 16:45:09 +0430

    add something

--- banner.php
+++ banner.php
@@ -3,7 +3,18 @@
 <head>
 <meta charset="utf-8">
 <title>Untitled Document</title>
+<link type="text/css" rel="stylesheet" href="banner.css" />
+<script type="text/javascript" src="banner.js"></script>
 </head>
 <body>
+<div class="container">
+	<div class="stuff">
+		<div class="tablo"></div>
+		<div class="iphone"></div>
+		<div class="key"></div>
+		<div class="mouse"></div>
+		<div class="mac"></div>
+	</div>
+</div>
 </body>
 </html>
